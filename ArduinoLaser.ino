ba4f7f04c4aa37f0d8187dbde96acf8035e86380
Sebastian Barkett <barkes2@rpi.edu>
2021-03-17 02:18:04 -0400

    Add files via upload

---
+++ ArduinoLaser.ino
@@ -0,0 +1,35 @@
+#include<Servo.h>
+
+Servo serX;
+Servo serY;
+String serialData;
+
+void setup() {
+  serX.attach(10);
+  serY.attach(11);
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+}
+
+void loop() {
+  // lol
+}
+
+void serialEvent(){
+  serialData = Serial.readString();
+
+  serX.write(parseDataX(serialData));
+  serY.write(parseDataY(serialData));
+}
+
+int parseDataX(String data){
+  data.remove(data.indexOf("Y"));
+  data.remove(data.indexOf("X"),1));
+
+  return data.toInt();
+}
+int parseDataY(String data){
+  data.remove(0, data.indexOf("Y" + 1));
+  
+  return data.toInt();
+}
