bb00cd98b0ff431fd309bca2b5437977588411e1
Sebastian Barkett <barkes2@rpi.edu>
2021-03-24 01:05:47 -0400

    Add files via upload

--- ArduinoDC.ino
+++ ArduinoDC.ino
@@ -1,15 +1,7 @@
-// cannot just plug dc motors into the arduino, because motor might draw more current than 
-//arduino can put out or motor can act as inductor that may cause a voltage spike 
-//arduino->transistor->motor->diode reversed bias to solve induction problem
-//motor controller chip can replace this method
-//all three need to share common GROUND: motor, motor controller chip, and power supply
-int motorPin = 3
-
+int motor = 3;
 void setup() {
-  pinMode(motorPin, OUTPUT);
+  pinMode(motor, OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(motorPIN, 255);
+  analogWrite(motor, 255);
 }
